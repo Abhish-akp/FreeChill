79990cdb996513fd996097af69e4936b678fd170
Abhishek Kumar Prajapti <akpabhishek3007@gmail.com>
2025-03-17 16:32:42 +0530

    Create Navbar

--- freechill-ui/src/pages/Freechill.jsx
+++ freechill-ui/src/pages/Freechill.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar';
 
 export default function Freechill() {
-  return (
-    <div>Freechill</div>
-  )
+
+    const [isScrolled , setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+      setIsScrolled(window.pageXOffset===0?false:true);
+      return () => (window.onscroll = null);
+    }
+     
+    <div>
+      <Navbar isScrolled={isScrolled}/>
+    </div>
 }
